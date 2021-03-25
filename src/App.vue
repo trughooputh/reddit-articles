be14b0ec5d9246a75bfebb09a82104f7b67d7293
Thomas Rughooputh <thomas.rughooputh@gmail.com>
2021-03-25 22:30:53 +0100

    Ad gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
+    <img alt="Vue logo" src="./assets/logo.png" width="5%">
     <HelloWorld msg="Hello Vue in CodeSandbox!"/>
     <List />
   </div>
